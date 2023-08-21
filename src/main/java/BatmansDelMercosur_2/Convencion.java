eb1d5a88453845a892ad9c81c280364dac70b140
Alexia Aroa <alexia.1509rojo@gmail.com>
2023-08-21 00:38:28 -0300

    Add Class Convencion

--- src/main/java/BatmansDelMercosur_2/Convencion.java
+++ src/main/java/BatmansDelMercosur_2/Convencion.java
@@ -1,4 +1,42 @@
 package BatmansDelMercosur_2;
 
+import java.util.ArrayList;
+
 public class Convencion {
+    private ArrayList<Batman> batmans = new ArrayList<>();
+
+    public ArrayList<Batman> losQueNecesitanTraductor() {
+        ArrayList<Batman> res = new ArrayList<>();
+
+        for (Batman b: batmans) {
+            if (b.necesitaTraductor()) {
+                res.add(b);
+            }
+        }
+
+        return res;
+    }
+
+    public boolean hayBatmanConAltoNivelFenS() {
+        for (Batman b: batmans) {
+            if (b.tieneAltoNivelFactEnSangre()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // PRE: El array de batmans no está vacío.
+    public Batman elQueComioMasFacturas() {
+        Batman res = batmans.get(0);
+
+        for (Batman b: batmans) {
+            if (b.getCantFacturasEnSangre() > res.getCantFacturasEnSangre()) {
+                res = b;
+            }
+        }
+
+        return res;
+    }
 }
